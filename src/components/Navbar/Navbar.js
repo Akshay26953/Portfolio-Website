c71de59293b79d4486fc21022db533d8c9b1c747
Akshay26953 <amkharat2695@gmail.com>
2022-10-15 00:53:00 +0530

    rename images

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
-// import { useEffect } from "react";
 
 function Navbar() {
   const [show, setShow] = useState();
