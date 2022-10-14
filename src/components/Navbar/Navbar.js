6567b5f4cdc0be2cbd20272e182a9e453a707943
Akshay26953 <amkharat2695@gmail.com>
2022-10-15 02:53:45 +0530

    navbar updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,26 +3,28 @@ import "./Navbar.scss";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  const [show, setShow] = useState();
+  const [show, setShow] = useState(false);
 
-  const handleCancle = () => {
-    return setShow("-100%");
-  };
-  const handleBurger = () => {
-    return setShow("0%");
-  };
-
+  // const handleCancle = () => {
+  //   return setShow("-100%");
+  // };
+  // const handleBurger = () => {
+  //   return setShow(true);
+  // };
+console.log(show)
   return (
     <div className="container contWidth">
       <div className="navbar">
         <div className="logo">
           <Link to="/">AkshayK.</Link>
         </div>
-        <div className="nav" style={{ 
-          transform: `translateX(${show})` 
+        <div className={show? "nav resNav": "nav"} 
+        style={{ 
+          // transform: `translateX(${show})` 
           }}>
           <div className="cancel" 
-          onClick={handleCancle}
+          // onClick={()=> console.log("cancel")}
+          onClick={()=> setShow(!show)}
           >
             <i className="fa-solid fa-xmark"></i>
           </div>
@@ -45,7 +47,9 @@ function Navbar() {
           </ul>
         </div>
         <div className="burger"
-         onClick={handleBurger}
+         onClick={()=> setShow(!show)}
+        // onClick={()=> console.log("burger")}
+
          >
           <i className="fa-solid fa-bars"></i>
         </div>
